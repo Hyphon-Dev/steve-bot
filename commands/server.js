72095ac3ef48e86b6c893baa2f792949ead3a089
Hyphon-Dev <hyphonwastaken@gmail.com>
2021-03-03 17:20:47 -0500

    Fixed Server

--- commands/server.js
+++ commands/server.js
@@ -6,16 +6,22 @@ exports.execute = (client, message, args) => {
     fetch('https://api.mcsrvstat.us/2/' + args[0])
     .then(res => res.json())
     .then(json => {
+        console.log(json)
         if(json.ip == "") {
             return sendError('The ip or hostname ' + args[0] + " doesn't exits!")
         } else {
+            fetch('https://api.mcsrvstat.us/icon/' + args[0])
+            .then(pic => {
             const ServerEmbed = new MessageEmbed()
             .setTitle(`${args[0]}'s server info`)
+            .setThumbnail(pic)
             .addField('Hostname:', json.hostname)
             .addField('Version:', json.version)
             .addField('Players:', `${json.players.online}/${json.players.max}`)
             .addField('MOTD:', json.motd.clean)
-        }
+            message.channel.send(ServerEmbed)
+            }
+        )}
     })
 }
 
