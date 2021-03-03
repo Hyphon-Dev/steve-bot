ba5d91bae0b16062dd56d6425812b71057e9d554
Hyphon-Dev <peakinox@outlook.com>
2021-03-02 23:17:10 -0500

    Update functions.js

--- util/functions.js
+++ util/functions.js
@@ -3,5 +3,5 @@ const { MessageEmbed } = require("discord.js")
 exports.sendError = (err, channel) => {
     const errorEmbed = new MessageEmbed()
     .setColor('RED')
-    .setTitle('ERROR')
+    .setTitle('❌ERROR')
 }
